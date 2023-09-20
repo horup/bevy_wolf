2dbf2962d12371e4db9a9c23725f0e69f90a43eb
= <horup@deepstacker.com>
2023-09-20 23:16:50 +0200

    vsync off

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,10 +1,16 @@
-use bevy::{prelude::*, render::texture::ImageSampler};
+use bevy::{prelude::*, render::texture::ImageSampler, window::PresentMode};
 use bevy_wolf::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin {
             default_sampler:ImageSampler::nearest_descriptor(),
+            ..Default::default()
+        }).set(WindowPlugin {
+            primary_window:Some(Window {
+                present_mode:PresentMode::Immediate,
+                ..Default::default()
+            }),
             ..Default::default()
         }))
         .add_plugins(WolfPlugin)
