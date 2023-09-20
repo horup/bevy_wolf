544be9bbc365a9d17684a177ea385d2df266abd3
Søren Alsbjerg Hørup <sah@sah.dk>
2023-09-20 08:08:22 +0200

    refactored

--- assets/tilesets/walls.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="walls" tilewidth="64" tileheight="64" tilecount="114" columns="6">
- <image source="../images/walls.png" width="384" height="1216"/>
-</tileset>
