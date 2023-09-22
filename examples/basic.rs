d949d5be0d999a0cbf8aa8a3fa4f8ea1c8cd4891
= <horup@deepstacker.com>
2023-09-22 08:36:16 +0200

    wip

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,4 +1,6 @@
-use bevy::{prelude::*, render::texture::ImageSampler, window::PresentMode};
+use std::{process::exit, slice::Windows};
+
+use bevy::{prelude::*, render::texture::ImageSampler, window::{PresentMode, Cursor, CursorGrabMode}};
 use bevy_wolf::*;
 
 fn main() {
@@ -9,13 +11,25 @@ fn main() {
         }).set(WindowPlugin {
             primary_window:Some(Window {
                 present_mode:PresentMode::Immediate,
+                cursor:Cursor {
+                    visible:false,
+                    grab_mode:CursorGrabMode::Locked,
+                    ..Default::default()
+                },
                 ..Default::default()
             }),
             ..Default::default()
         }))
         .add_plugins(WolfPlugin)
         .add_systems(Startup, startup_system)
+        .add_systems(Update, input_system)
         .run();
+}
+
+fn input_system(keys:Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::Escape) {
+        exit(0);
+    }
 }
 
 fn startup_system(mut world:ResMut<WolfWorld>, ass:Res<AssetServer>, ) {
